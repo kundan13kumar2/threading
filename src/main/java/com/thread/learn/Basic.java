cb047b0194d86b5637fb9a47e0ce342352f6312a
kundan13kumar2 <kundan94.mnnit@gmail.com>
2019-03-05 20:21:01 +0530

    Hello Thread

--- src/main/java/com/thread/learn/Basic.java
+++ src/main/java/com/thread/learn/Basic.java
@@ -1,4 +1,13 @@
 package com.thread.learn;
 
-public class Basic {
+public class Basic extends Thread {
+
+    public void run() {
+        System.out.println("Welcome to Threading World. " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread th1 = new Thread(new Basic(), "A");
+        th1.start();
+    }
 }
