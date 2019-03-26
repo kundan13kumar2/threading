66762e3e185355901e0de0d6e0637a7517b65f5a
kundan13kumar2 <kundan.k@appnomic.com>
2019-03-26 14:15:45 +0530

    +- commit

---
+++ src/main/java/com/preparation/CHEFCH.java
@@ -0,0 +1,67 @@
+package com.preparation;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CHEFCH {
+
+
+    public static int count(String str) {
+
+        char[] charArray = str.toCharArray();
+
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+
+        for (Character ch : charArray) {
+
+
+            if (ch == '-' && !st1.empty() && st1.peek() == '+')
+                st1.pop();
+            else
+                st1.push(ch);
+
+
+            if (ch == '+' && !st2.empty() && st2.peek() == '-')
+                st2.pop();
+            else
+                st2.push(ch);
+
+        }
+
+        int ap = 0, am = 0, bp = 0, bm = 0;
+
+        while (!st1.empty()) {
+            if (st1.peek() == '+')
+                ap++;
+            else
+                am++;
+            st1.pop();
+        }
+
+        while (!st2.empty()) {
+            if (st2.peek() == '+')
+                bp++;
+            else
+                bm++;
+            st2.pop();
+        }
+
+        return Math.max(ap / 2 + am / 2, bp / 2 + bm / 2);
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.next());
+
+        while (t > 0) {
+            String string = scanner.next();
+            System.out.println(count(string));
+            t--;
+        }
+
+    }
+
+}
