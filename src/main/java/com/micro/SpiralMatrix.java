e6da72a43ab90f3b7117adfc1920fcf9e48c2a58
kundan13kumar2 <kundan94.mnnit@gmail.com>
2021-09-17 14:30:41 +0530

    few codes

--- src/main/java/com/micro/SpiralMatrix.java
+++ src/main/java/com/micro/SpiralMatrix.java
@@ -2,26 +2,30 @@ package com.micro;
 
 public class SpiralMatrix {
     public static void main(String[] args) {
-        int[][] mat = {{1, 2, 3, 4, 5}, {16, 17, 18, 19, 6}, {15, 24, 25, 20, 7}, {14, 23, 22, 21, 8}, {13, 12, 11, 10, 9}};
+        int[][] mat = {{1, 2, 3}};
 
         int r = 0;
         int c = 0;
-        int row = 5;
-        int col = 5;
+        int row = 1;
+        int col = 3;
 
-        while (row >= r && col >= c) {
+        while (row > r && col > c) {
             for (int i = c; i < col; i++)
                 System.out.println(mat[r][i]);
             r++;
             for (int i = r; i < row; i++)
                 System.out.println(mat[i][col - 1]);
             col--;
-            for (int i = col - 1; i >= c; i--)
-                System.out.println(mat[row - 1][i]);
-            row--;
-            for (int i = row - 1; i >= r; i--)
-                System.out.println(mat[i][c]);
-            c++;
+            if (r < row) {
+                for (int i = col - 1; i >= c; i--)
+                    System.out.println(mat[row - 1][i]);
+                row--;
+            }
+            if (c < col) {
+                for (int i = row - 1; i >= r; i--)
+                    System.out.println(mat[i][c]);
+                c++;
+            }
         }
     }
 }
